50873e666a981f53d510430b6938f87bfa3e4e81
Bananop <anasprada1612@gmail.com>
2026-05-03 12:28:20 -0500

    login service

--- src/app/(public)/login/services/login.service.ts
+++ src/app/(public)/login/services/login.service.ts
@@ -1,7 +1,12 @@
-import apiClient from '../../../../lib/axios/client';
-import type { LoginDto, AuthResponse } from '../../../../types';
+import apiClient from '@/lib/axios/client';
+import type { LoginDto, AuthResponse, User } from '@/types';
 
 export const loginUser = async (credentials: LoginDto): Promise<AuthResponse> => {
     const { data } = await apiClient.post<AuthResponse>('/auth/login', credentials);
+    return data;
+};
+
+export const getUserByEmail = async (email: string): Promise<User> => {
+    const { data } = await apiClient.get<User>(`/users/email/${email}`);
     return data;
 };
